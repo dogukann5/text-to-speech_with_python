6e74201e51a0910429f01e87674eb0cf2d81e7e7
dotadev <dogukantabak@hotmail.com>
2023-07-14 22:26:27 +0300

    Update

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request, send_file
-from gtts import gTTS
+from gtts import gTTS, lang
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    languages = lang.tts_langs()
+    return render_template('index.html', languages=languages)
 
 @app.route('/download', methods=['POST'])
 def download():
     text = request.form['text']
-    tts = gTTS(text)
+    language = request.form['language']
+    tts = gTTS(text, lang=language)
     tts.save('output.mp3')
     return send_file('output.mp3', as_attachment=True)
 
