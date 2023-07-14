576ed4c7a0fdbe87d0a05dd78549c5b3367226fe
dotadev <dogukantabak@hotmail.com>
2023-07-14 22:58:11 +0300

    update

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, send_file
 from gtts import gTTS, lang
-from io import BytesIO
 
 app = Flask(__name__)
 
@@ -9,15 +8,13 @@ def index():
     languages = lang.tts_langs()
     return render_template('index.html', languages=languages)
 
-@app.route('/preview', methods=['POST'])
-def preview():
+@app.route('/download', methods=['POST'])
+def download():
     text = request.form['text']
     language = request.form['language']
     tts = gTTS(text, lang=language)
-    mp3_fp = BytesIO()
-    tts.save(mp3_fp)
-    mp3_fp.seek(0)
-    return send_file(mp3_fp, as_attachment=True)
+    tts.save('output.mp3')
+    return send_file('output.mp3', as_attachment=True)
 
 if __name__ == '__main__':
     app.run()
